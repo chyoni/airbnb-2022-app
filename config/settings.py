2f2651661dd7ec0a0fafd51df8dbe21ef096a903
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2022-08-09 13:01:53 +0900

    renderer api browser set

--- config/settings.py
+++ config/settings.py
@@ -155,3 +155,9 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
 }
+
+
+if not DEBUG:
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
+        "rest_framework.renderers.JSONRenderer"
+    ]
